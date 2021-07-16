65c4e87758884158151d29b69a534995b903589c
Santeri Einola <einola.santeri@gmail.com>
2021-07-16 16:29:52 +0300

    background fix

--- src/App.js
+++ src/App.js
@@ -8,26 +8,49 @@ import {
 } from 'react-router-dom'
 import Details from './components/Details'
 import Info from './components/Info'
+import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
 
 const App = () => {
 
   return (
     <Router>
-      <div className='App'>
-        <Nav />
+      <BrowserView>
+        <div className='App'>
+          <Nav />
 
-        <Switch>
-          <Route path="/info">
-            <Info />
-          </Route>
-          <Route path="/details">
-              <Details />
-          </Route>
-          <Route path="/">
-            <NestedGrid />
-          </Route>
-        </Switch>
-      </div>
+          <Switch>
+            <Route path="/info">
+              <Info />
+            </Route>
+            <Route path="/details">
+                <Details />
+            </Route>
+            <Route path="/">
+              <NestedGrid />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className='App__mobile'>
+          <Nav />
+
+          <Switch>
+            <Route path="/info">
+              <Info />
+            </Route>
+            <Route path="/details">
+                <Details />
+            </Route>
+            <Route path="/">
+              <NestedGrid />
+            </Route>
+          </Switch>
+        </div>
+      </MobileView>
     </Router>
   )
 }
